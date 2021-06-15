553b1f4179b80719ff3c7a571e6774f38817a79f
Manav <manavguptamg1990@gmail.com>
2021-06-15 23:51:48 +0530

    Day 7

---
+++ THA7/app.js
@@ -0,0 +1,21 @@
+//Q1
+var student={
+    name : "David Rayy", sclass : "VI", rollno : 12
+}
+
+console.log(student.name);
+console.log(student.sclass);
+console.log(student.rollno);
+
+// Q2
+delete student.rollno
+console.log(student)
+
+
+//Q3
+var student = { 
+    name : "David Rayy",
+     sclass : "VI",
+      rollno : 12
+     }; 
+console.log((Object.keys(student).length))
