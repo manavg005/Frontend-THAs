bb66d601047c3b19084da60a2e635ca419ee8141
manavg005 <manavguptamg1990@gmail.com>
2021-08-07 16:59:55 +0530

    THA 24

---
+++ THA 24/src/profile.jsx
@@ -0,0 +1,13 @@
+
+const Profile = ()=>{
+    return<div className="profile page-container">
+        <div className="title">
+            PROFILE
+        </div>
+        <div className="content">
+            Private Route
+        </div>
+    </div>
+}
+
+export default Profile;
